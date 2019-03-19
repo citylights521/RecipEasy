365a6d9d9397012fdbbbb0eb7c8aa93274f55733
Turbocake64 <micahrabinowitz@gmail.com>
2019-03-19 10:34:16 -0700

    edits to the card to bring up the map

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,32 +45,54 @@ $(document).ready(function () {
         randomRecipesDiv.append(recipeDiv);
     };
 
+    // onclick function for a given recipe image to move to stage 2
+    // Creating a single card for a chosen recipe
     $(".card-img-top").on("click", function () {
+        // this empties the div populated by the search function
+        $("#ramdomRecipes").empty();
+        // console log the click function of the images
         console.log("clicked a card"); 
+        // declares the variable chosenRecipeDiv 
     var chosenRecipeDiv = $("#chosenRecipe");
-        // data-title is tentative
+        // data-title is tentative.
         const chosenTitle = $(this).attr(data-title);
-        const chosenPicSrc = picSrc;
+        //declares the 
+        const chosenPicSrc = recipeImage;
         // outer div column
         var chosenRecipeDiv = $("<div>");
         chosenRecipeDiv.attr("class", "col-md chosenRecipeDiv");
-        //inner recipe div
+        // inner recipe div
         var chosenRecipeCard = $("<div>");
         chosenRecipeCard.attr("class", "card chosenRecipeCard");
-        //generates recipe card image
+        // generates recipe card image
         var chosenRecipeImage = $("<img>");
+        // should create the top of the card and make it an image
         chosenRecipeImage.attr("class", "card-img-top");
+        // should define the picture selected for onclick as the image in card-img-top
         chosenRecipeImage.attr("src", chosenPicSrc);
+        // should set the picture selected for onclick as the image in card-img-top
         chosenRecipeCard.append(chosenRecipeImage);
+        // creating the cardBody div
         var cardBody = $("<div>");
+        // gives cardbody the class of card-body
         cardBody.attr("class", "card-body");
+        // add the title from the API to the stage 2 card
+        cardBody.append(chosenTitle);
         // ingredients is a placeholder. It needs to be connected to the API
-        // this may not work
+        // this may not work, needs testing
         for (let index = 0; index < ingredients.length; index++) {
             cardBody.append('<input type="checkbox" /> ' + ingredient[i] + '<br />')
         };
-        var mapButton = $('<input/>').attr({ type: 'button', name:'mapBtn', value:'Get the Goods' });
-        chosenRecipeCard.append(mapButton)
+        // declares a button to take you to the map (stage 2.5)
+        var mapButton = $('<input/>').attr({ type: 'button', name:'mapBtn', id:'mapBtn', value:'Get the Goods' });
+        // adds the button to the card
+        chosenRecipeCard.append(mapButton);
+        // a click function to change the recipe image to a dummy image of google maps
+        $("#mapBtn").on("click", function() {
+            $("#my_image").attr("src", "../mock-up-map.png");
+        })
+
     });
+
 
 });
