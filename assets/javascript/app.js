f0afc38cece1e6778c54ab8cfa3c5a8275fb9c76
Claire Gibeau <45344355+citylights521@users.noreply.github.com>
2019-03-19 11:12:45 -0700

    Revert "Splitchar title"

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,17 +30,6 @@ $(window).on("load", function () {
 
     //click handlers for recipe buttons go here
 
-splitcharTitle
-//fake data to make random recipe cards appear
-var fakeRecipeTitle = ["pizza", "ice cream", "candy", "cake", "pizza", "ice cream", "candy", "cake"];
-var fakeRecipePic = ["./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg", "./assets/images/blue-colors-cream-928475.jpg"];
-
-$(document).ready(function () {
-    //enables splitchar to print to page
-    $(".splitchar").splitchar();
-    
-    //TODO: plug in API call to random recipes here!!! Insert over fakeRecipe arrays (title and pic)
-
     //on enter/submit, run findRecipe with search 
     $("#getRecipe").submit(findRecipe);
     // $("#getRecipe").val()
@@ -48,7 +37,6 @@ $(document).ready(function () {
     //     event.preventDefault();
     //     console.log("something");
     // });
-
 
 })
 ///////////////////
